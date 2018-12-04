cd7d2527404cea9baa99a0fd994d3a972e2794d2
NGUYEN TAN CANH <nguye.tan.canh@gmail.com>
2018-12-04 23:15:44 +0700

    create server commit 2

---
+++ index.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var app = express();
+var ExpressPeerServer = require('peer').ExpressPeerServer;
+
+app.get('/', function(req, res, next) { res.send('Hello world!'); });
+
+// =======
+
+// var server = app.listen(9000, () => console.log(`start port 9000`));
+
+var options = {
+    debug: true
+}
+
+// var peerserver = ExpressPeerServer(server, options);
+
+// app.use('/api', peerserver);
+
+// == OR ==
+
+var server = require('http').createServer(app);
+var peerserver = ExpressPeerServer(server, options);
+
+app.use('/peerjs', peerserver);
+
+server.listen(9000, () => console.log(`start port 9000`));
+
+// ========
